470b63442828ca8cfda06fc0457f2483033b5f9e
Julien Fontanet <julien.fontanet@isonoe.net>
2018-01-18 18:26:03 +0100

    feat: testing support

--- README.md
+++ README.md
@@ -10,7 +10,6 @@ Differences with [freactal](https://github.com/FormidableLabs/freactal/):
 - no [helper functions](https://github.com/FormidableLabs/freactal#helper-functions) (not necessary)
 - no [hydrate support](https://github.com/FormidableLabs/freactal#hydrate-and-initialize)
 - no [middlewares support](https://github.com/FormidableLabs/freactal/#middleware)
-- no [test instances support](https://github.com/FormidableLabs/freactal/#state-and-effects)
 
 ## Install
 
@@ -63,6 +62,161 @@ import factory from '@julien-f/freactal/factory'
 const { injectState, provideState } = factory(Preact)
 
 // The rest is the same.
+```
+
+### Testing
+
+> This example comes from the excellent [Freactal documentation](https://github.com/FormidableLabs/freactal/#testing).
+
+`App.js`:
+
+```js
+import React from 'react'
+import { injectState, provideState } from '@julien-f/freactal'
+
+export default provideState({
+  initialState: () => ({
+    givenName: 'Walter',
+    familyName: 'Harriman',
+  }),
+  effects: {
+    onChangeGiven: (_, { target: { value } }) => state => ({
+      ...state,
+      givenName: value,
+    }),
+    onChangeFamily: (_, { target: { value } }) => state => ({
+      ...state,
+      familyName: value,
+    }),
+  },
+  computed: {
+    fullName: ({ givenName, familyName }) => `${givenName} ${familyName}`,
+    greeting: ({ fullName }) => `Hi, ${fullName}, and welcome!`,
+  },
+})(
+  injectState(({ state, effects }) => (
+    <div>
+      <p id="greeting">{state.greeting}</p>
+      <p>
+        <label>
+          Enter your given name:{' '}
+          <input
+            id="given"
+            onChange={effects.onChangeGiven}
+            value={state.givenName}
+          />
+        </label>
+      </p>
+      <p>
+        <label>
+          Enter your family name:{' '}
+          <input
+            id="family"
+            onChange={effects.onChangeFamily}
+            value={state.familyName}
+          />
+        </label>
+      </p>
+    </div>
+  ))
+)
+```
+
+`App.spec.js`:
+
+```js
+import React from 'react'
+import Adapter from 'enzyme-adapter-react-16'
+import { configure, mount } from 'enzyme'
+
+configure({ adapter: new Adapter() })
+
+import AppWithState from './App.js'
+
+describe('my app', () => {
+  // From the decorated component you can obtained the plain version
+  // directly.
+  const App = AppWithState.WrappedComponent
+
+  // We'll be re-using these values, so let's put it here for convenience.
+  const state = {
+    givenName: 'Charlie',
+    familyName: 'In-the-box',
+    fullName: 'Charlie In-the-box',
+    greeting: 'Howdy there, kid!',
+  }
+
+  it('displays a greeting to the user', () => {
+    // This test should be easy - all we have to do is ensure that
+    // the string that is passed in is displayed correctly!
+
+    // We're not doing anything with effects here, so let's not bother
+    // setting them for now...
+    const effects = {}
+
+    // First, we mount the component, providing the expected state and effects.
+    const el = mount(<App state={state} effects={effects} />)
+
+    // And then we can make assertions on the output.
+    expect(el.find('#greeting').text()).toBe('Howdy there, kid!')
+  })
+
+  it('accepts changes to the given name', () => {
+    // Next we're testing the conditions under which our component might
+    // interact with the provided effects.
+    const effects = {
+      onChangeGiven: jest.fn(),
+      onChangeFamily: jest.fn(),
+    }
+
+    const el = mount(<App state={state} effects={effects} />)
+
+    // We don't expect our effect to be invoked when the component
+    // mounts, so let's make that assertion here.
+    expect(effects.onChangeGiven).not.toHaveBeenCalled()
+
+    // Next, we can simulate a input-box value change.
+    el.find('input#given').simulate('change', {
+      target: { value: 'Eric' },
+    })
+
+    // And finally, we can assert that the effect - or, rather, the Sinon
+    // spy that is standing in for the effect - was invoked with the expected
+    // value.
+    expect(effects.onChangeGiven).toHaveBeenCalledWith(
+      expect.objectContaining({
+        target: {
+          value: 'Eric',
+        },
+      })
+    )
+  })
+})
+
+describe('my app state', () => {
+  // From the decorated component you can obtained the decorator
+  // providing the state directly.
+  const { wrapComponentWithState } = AppWithState
+
+  it('supports fullName', async () => {
+    // Normally, you'd pass a component as the first argument to your
+    // state template.  However, if you pass no argument to the state
+    // template, you'll get back a test instance that you can extract
+    // `state` and `effects` from.  Just don't try to render the thing!
+    const { effects, getState } = wrapComponentWithState()
+
+    expect(getState().fullName).toBe('Walter Harriman')
+
+    await effects.onChangeGiven({ target: { value: 'Alfred' } })
+    expect(getState().fullName).toBe('Alfred Harriman')
+
+    await effects.onChangeFamily({ target: { value: 'Hitchcock' } })
+    expect(getState().fullName).toBe('Alfred Hitchcock')
+  })
+
+  // You could write similar assertions here
+  it('supports a greeting')
+})
 ```
 
 ## Development
--- src/factory.js
+++ src/factory.js
@@ -46,6 +46,8 @@ module.exports = ({ Component, createElement, PropTypes }) => {
 
   const injectState = ChildComponent =>
     class StateInjector extends Component {
+      static WrappedComponent = ChildComponent;
+
       static contextTypes = contextTypes_;
 
       constructor (_, context) {
@@ -83,169 +85,183 @@ module.exports = ({ Component, createElement, PropTypes }) => {
       }
     }
 
-  const provideState = ({
-    computed,
-    effects,
-    initialState,
-  }) => ChildComponent =>
-    class StateProvider extends Component {
-      static childContextTypes = contextTypes_;
-      static contextTypes = contextTypes_;
+  const provideState = ({ computed, effects, initialState }) => {
+    const wrapComponentWithState = ChildComponent => {
+      class StateProvider extends Component {
+        static WrappedComponent = (ChildComponent != null &&
+          ChildComponent.WrappedComponent) ||
+          ChildComponent;
+        static wrapComponentWithState = wrapComponentWithState;
 
-      constructor (props, context) {
-        super()
+        static childContextTypes = contextTypes_;
+        static contextTypes = contextTypes_;
 
-        const listeners = new Set()
-        const dispatch = (this._dispatch = () => {
-          // computedCache.clear()
-          listeners.forEach(call)
-        })
-        this._subscribe = listener => {
-          listeners.add(listener)
-          return () => {
-            listeners.delete(listener)
-          }
-        }
+        constructor (props, context) {
+          super()
 
-        const stateDescriptors = create(null)
-
-        if (computed !== undefined) {
-          const propsKeys = keys(props)
-          const propsAccessor = k => this.props[k]
-          const stateAccessor = k => completeState[k]
-
-          keys(computed).forEach(k => {
-            const c = computed[k]
-            let previousValue, propsProxy, propsSpy, stateProxy, stateSpy
-            stateDescriptors[k] = {
-              get: () => {
-                if (propsProxy === undefined) {
-                  [propsProxy, propsSpy] = makeSpy(propsKeys, propsAccessor);
-                  [stateProxy, stateSpy] = makeSpy(
-                    completeStateKeys,
-                    stateAccessor
-                  )
-                } else if (propsSpy.upToDate() && stateSpy.upToDate()) {
-                  return previousValue
-                }
-                propsSpy.clear()
-                stateSpy.clear()
-                return (previousValue = c(stateProxy, propsProxy))
-              },
-            }
+          const listeners = new Set()
+          const dispatch = (this._dispatch = () => {
+            // computedCache.clear()
+            listeners.forEach(call)
           })
-        }
+          this._subscribe = listener => {
+            listeners.add(listener)
+            return () => {
+              listeners.delete(listener)
+            }
+          }
 
-        let state
-        if (initialState !== undefined) {
-          state = initialState(props)
+          const stateDescriptors = create(null)
 
-          keys(state).forEach(k => {
-            if (!(k in stateDescriptors)) {
-              // only local, non-computed state entries are enumerable
+          if (computed !== undefined) {
+            const propsKeys = keys(props)
+            const propsAccessor = k => this.props[k]
+            const stateAccessor = k => completeState[k]
+
+            keys(computed).forEach(k => {
+              const c = computed[k]
+              let previousValue, propsProxy, propsSpy, stateProxy, stateSpy
               stateDescriptors[k] = {
+                get: () => {
+                  if (propsProxy === undefined) {
+                    [propsProxy, propsSpy] = makeSpy(propsKeys, propsAccessor);
+                    [stateProxy, stateSpy] = makeSpy(
+                      completeStateKeys,
+                      stateAccessor
+                    )
+                  } else if (propsSpy.upToDate() && stateSpy.upToDate()) {
+                    return previousValue
+                  }
+                  propsSpy.clear()
+                  stateSpy.clear()
+                  return (previousValue = c(stateProxy, propsProxy))
+                },
+              }
+            })
+          }
+
+          let state
+          if (initialState !== undefined) {
+            state = initialState(props)
+
+            keys(state).forEach(k => {
+              if (!(k in stateDescriptors)) {
+                // only local, non-computed state entries are enumerable
+                stateDescriptors[k] = {
+                  enumerable: true,
+                  get: () => state[k],
+                }
+              }
+            })
+          }
+
+          let parentEffects = null
+          let parentState = null
+          let parentStateKeys = []
+          {
+            const parent = context[TAG]
+            if (parent !== undefined) {
+              parentEffects = parent.effects
+              parentState = parent.state
+              parentStateKeys = parent.stateKeys
+            }
+          }
+
+          const completeState = (this._state = create(
+            parentState,
+            stateDescriptors
+          ))
+          const completeStateKeys = (this._stateKeys = keys(stateDescriptors))
+
+          parentStateKeys.forEach(k => {
+            if (!(k in stateDescriptors)) {
+              completeStateKeys.push(k)
+            }
+          })
+
+          let effectsDescriptors
+          if (effects !== undefined) {
+            const setState = updater => {
+              if (updater !== undefined) {
+                const newState = updater(completeState, this.props)
+                if (newState != null) {
+                  state = newState
+                  dispatch()
+                }
+              }
+            }
+
+            effectsDescriptors = create(null)
+            keys(effects).forEach(k => {
+              const e = effects[k]
+              effectsDescriptors[k] = {
                 enumerable: true,
-                get: () => state[k],
+                value: (...args) => {
+                  return new Promise(resolve =>
+                    resolve(e(this._effects, ...args))
+                  ).then(setState)
+                },
               }
-            }
-          })
+            })
+          }
+          this._effects = create(parentEffects, effectsDescriptors)
         }
 
-        let parentEffects = null
-        let parentState = null
-        let parentStateKeys = []
-        {
-          const parent = context[TAG]
+        _unsubscribe = noop;
+
+        componentDidMount () {
+          const parent = this.context[TAG]
           if (parent !== undefined) {
-            parentEffects = parent.effects
-            parentState = parent.state
-            parentStateKeys = parent.stateKeys
+            this._unsubscribe = parent.subscribe(this._dispatch)
+          }
+
+          const { initialize } = this._effects
+          if (initialize !== undefined) {
+            initialize()
           }
         }
 
-        const completeState = (this._state = create(
-          parentState,
-          stateDescriptors
-        ))
-        const completeStateKeys = (this._stateKeys = keys(stateDescriptors))
+        componentDidUpdate () {
+          this._dispatch()
+        }
 
-        parentStateKeys.forEach(k => {
-          if (!(k in stateDescriptors)) {
-            completeStateKeys.push(k)
-          }
-        })
-
-        let effectsDescriptors
-        if (effects !== undefined) {
-          const setState = updater => {
-            if (updater !== undefined) {
-              const newState = updater(completeState, this.props)
-              if (newState != null) {
-                state = newState
-                dispatch()
-              }
-            }
+        componentWillUnmount () {
+          const { finalize } = this._effects
+          if (finalize !== undefined) {
+            finalize()
           }
 
-          effectsDescriptors = create(null)
-          keys(effects).forEach(k => {
-            const e = effects[k]
-            effectsDescriptors[k] = {
-              enumerable: true,
-              value: (...args) => {
-                return new Promise(resolve =>
-                  resolve(e(this._effects, ...args))
-                ).then(setState)
-              },
-            }
-          })
-        }
-        this._effects = create(parentEffects, effectsDescriptors)
-      }
-
-      _unsubscribe = noop;
-
-      componentDidMount () {
-        const parent = this.context[TAG]
-        if (parent !== undefined) {
-          this._unsubscribe = parent.subscribe(this._dispatch)
+          this._unsubscribe()
+          this._unsubscribe = noop
         }
 
-        const { initialize } = this._effects
-        if (initialize !== undefined) {
-          initialize()
+        getChildContext () {
+          return {
+            [TAG]: {
+              effects: this._effects,
+              state: this._state,
+              stateKeys: this._stateKeys,
+              subscribe: this._subscribe,
+            },
+          }
+        }
+
+        render () {
+          return createElement(ChildComponent, this.props)
         }
       }
 
-      componentDidUpdate () {
-        this._dispatch()
-      }
-
-      componentWillUnmount () {
-        const { finalize } = this._effects
-        if (finalize !== undefined) {
-          finalize()
-        }
-
-        this._unsubscribe()
-        this._unsubscribe = noop
-      }
-
-      getChildContext () {
+      if (ChildComponent === undefined) {
+        const stateProvider = new StateProvider({}, {})
         return {
-          [TAG]: {
-            effects: this._effects,
-            state: this._state,
-            stateKeys: this._stateKeys,
-            subscribe: this._subscribe,
-          },
+          getState: () => stateProvider._state,
+          effects: stateProvider._effects,
         }
       }
-
-      render () {
-        return createElement(ChildComponent, this.props)
-      }
+      return StateProvider
     }
+    return wrapComponentWithState
+  }
 
   return { injectState, provideState }
 }
