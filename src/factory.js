c486c33816d8a136a92bcc8667dd3e3a284b2385
Mohamedox <ghemid_m_dz@yahoo.fr>
2019-02-27 11:01:57 +0100

    fix passing props to initialState

--- src/factory.js
+++ src/factory.js
@@ -195,7 +195,7 @@ module.exports = ({ Component, createElement, PropTypes }) => {
           const completeStateKeys = (this._stateKeys = keys(stateDescriptors))
 
           this._resetState = () => {
-            const _initialstate = initialState(props)
+            const _initialstate = initialState(this.props)
             keys(_initialstate).forEach(k => {
               if (k in stateDescriptors) {
                 this._state[k] = _initialstate[k]
