34eeeb1339517348c625f3a638a5acefe07d220c
Julien Fontanet <julien.fontanet@isonoe.net>
2020-06-01 11:10:45 +0200

    fix(InvalidEntryError): typo

--- src/_InvalidEntryError.js
+++ src/_InvalidEntryError.js
@@ -3,7 +3,7 @@ import { BaseError } from "make-error";
 export default class InvalidEntryError extends BaseError {
   constructor(name) {
     super(
-      `"${name}" is not a valid state entry. If you want to use it, initialize it in "intialState"`
+      `"${name}" is not a valid state entry. If you want to use it, initialize it in "initialState"`
     );
   }
 }
